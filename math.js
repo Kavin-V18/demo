5d5f2248c81887d6e13c963e6351d92851767cbf
Kavin-V18 <kavin.v@mitrahsoft.in>
2026-03-19 12:53:27 +0530

    first commit

---
+++ math.js
@@ -0,0 +1,19 @@
+function add(a,b){
+    console.log(a+b)
+}
+function sub(a,b){
+    console.log(a-b)
+}
+function multiply(a,b){
+    return a*b
+}
+function divide(a,b) {
+    console.log(a/b)
+}
+
+let result=multiply(10,20)
+console.log(result)
+
+add(10,20)
+sub(10,20)
+divide(20,10)
