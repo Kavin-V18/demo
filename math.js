4bffa2fa6b994e91298f17ea1128bec9466ac487
Kavin-V18 <kavin.v@mitrahsoft.in>
2026-03-26 12:22:28 +0530

    math.js modified with cherry-pick commit

--- math.js
+++ math.js
@@ -7,12 +7,8 @@ function sub(a,b){
 function multiply(a,b){
     return a*b
 }
-function divide(a,b) {
-    console.log(a/b)
-}
 
-let result=multiply(10,20)
-console.log(result)
+
 
 add(10,20)
 sub(10,20)
